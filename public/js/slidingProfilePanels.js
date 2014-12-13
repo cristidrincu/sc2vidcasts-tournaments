c71fc5580c0501ca443e31d42e71745cbd767a4e
Cristian Sergiu Drincu <cristidrincu@gmail.com>
2014-12-13 21:46:03 +0200

    Implemented a new design for user dashboard

--- public/js/slidingProfilePanels.js
+++ public/js/slidingProfilePanels.js
@@ -3,5 +3,6 @@
  */
 $(document).ready(function(){
 	$('.sliding-panel-main-profile, .div-gradient, .div-gradient-red, .div-gradient-green').hide();
-	$('.sliding-panel-main-profile, .div-gradient, .div-gradient-red, .div-gradient-green').slideDown(1000);
+	$('.sliding-panel-main-profile').show();
+	$('.div-gradient, .div-gradient-red, .div-gradient-green').slideDown(1000);
 });

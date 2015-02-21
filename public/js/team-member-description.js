f4b185d7f6212c926d769ebe0c3be550e8e7c889
Cristian Sergiu Drincu <cristidrincu@gmail.com>
2015-02-21 17:32:56 +0200

    Added sections for players, organizers and tournaments to the frontend view

--- public/js/team-member-description.js
+++ public/js/team-member-description.js
@@ -10,12 +10,16 @@ $(document).ready(function(){
 
 	var cristianDescription = $('.cristian-description');
 	var adilaDescription = $('.adila-description');
+	var hundreiDescription = $('.hundrei-description');
+	var bouaruDescription = $('.bouaru-description');
+	var digitalDescription = $('.digital-description');
 
 	defaultDescription.stop(true, true).fadeIn(2000).show();
 
 	$('#cristian').on({
 		click: function(){
-			$(defaultActiveImage).removeClass('.active-image').css({'-webkit-filter': 'grayscale(80%)','filter': 'grayscale(80%)'});
+			$(this).attr('status', 'clicked');
+			$(defaultActiveImage).removeClass('.active-image').attr('status', 'unclicked').css({'-webkit-filter': 'grayscale(80%)','filter': 'grayscale(80%)'});
 			defaultActiveImage = $(this);
 			$(this).css({'-webkit-filter': 'grayscale(0%)','filter': 'grayscale(0%)'}).addClass('.active-image');
 
@@ -24,13 +28,21 @@ $(document).ready(function(){
 			defaultDescription = $(cristianDescription);
 		},
 		mouseenter: function(){
-			$(this).css({'-webkit-filter': 'grayscale(0%)','filter': 'grayscale(0%)'});
+			if($(this).attr('status') != 'clicked'){
+				$(this).css({'-webkit-filter': 'grayscale(0%)','filter': 'grayscale(0%)'});
+			}
+		},
+		mouseleave: function(){
+			if($(this).attr('status') != 'clicked'){
+				$(this).css({'-webkit-filter': 'grayscale(80%)','filter': 'grayscale(80%)'});
+			}
 		}
 	});
 
 	$('#adila').on({
 		click: function(){
-			$(defaultActiveImage).removeClass('.active-image').css({'-webkit-filter': 'grayscale(80%)','filter': 'grayscale(80%)'});
+			$(this).attr('status', 'clicked');
+			$(defaultActiveImage).removeClass('.active-image').attr('status','unclicked').css({'-webkit-filter': 'grayscale(80%)','filter': 'grayscale(80%)'});
 			defaultActiveImage = $(this);
 			$(this).css({'-webkit-filter': 'grayscale(0%)','filter': 'grayscale(0%)'}).addClass('.active-image');
 
@@ -38,9 +50,84 @@ $(document).ready(function(){
 			$(adilaDescription).stop(true, true).fadeIn(2000).show().addClass('.active-description');
 			defaultDescription = $(adilaDescription);
 		},
-		hover: function(){
-			$(this).css({'-webkit-filter': 'grayscale(0%)','filter': 'grayscale(0%)'});
-			$(this).css({'-webkit-filter': 'grayscale(80%)','filter': 'grayscale(80%)'});
+		mouseenter: function(){
+			if($(this).attr('status') != 'clicked'){
+				$(this).css({'-webkit-filter': 'grayscale(0%)','filter': 'grayscale(0%)'});
+			}
+		},
+		mouseleave: function(){
+			if($(this).attr('status') != 'clicked'){
+				$(this).css({'-webkit-filter': 'grayscale(80%)','filter': 'grayscale(80%)'});
+			}
+		}
+	});
+
+	$('#hundrei').on({
+		click: function(){
+			$(this).attr('status', 'clicked');
+			$(defaultActiveImage).removeClass('.active-image').attr('status','unclicked').css({'-webkit-filter': 'grayscale(80%)','filter': 'grayscale(80%)'});
+			defaultActiveImage = $(this);
+			$(this).css({'-webkit-filter': 'grayscale(0%)','filter': 'grayscale(0%)'}).addClass('.active-image');
+
+			defaultDescription.stop(true, true).hide().removeClass('.active-description');
+			$(hundreiDescription).stop(true, true).fadeIn(2000).show().addClass('.active-description');
+			defaultDescription = $(hundreiDescription);
+		},
+		mouseenter: function(){
+			if($(this).attr('status') != 'clicked'){
+				$(this).css({'-webkit-filter': 'grayscale(0%)','filter': 'grayscale(0%)'});
+			}
+		},
+		mouseleave: function(){
+			if($(this).attr('status') != 'clicked'){
+				$(this).css({'-webkit-filter': 'grayscale(80%)','filter': 'grayscale(80%)'});
+			}
+		}
+	});
+
+	$('#bouaru').on({
+		click: function(){
+			$(this).attr('status', 'clicked');
+			$(defaultActiveImage).removeClass('.active-image').attr('status','unclicked').css({'-webkit-filter': 'grayscale(80%)','filter': 'grayscale(80%)'});
+			defaultActiveImage = $(this);
+			$(this).css({'-webkit-filter': 'grayscale(0%)','filter': 'grayscale(0%)'}).addClass('.active-image');
+
+			defaultDescription.stop(true, true).hide().removeClass('.active-description');
+			$(bouaruDescription).stop(true, true).fadeIn(2000).show().addClass('.active-description');
+			defaultDescription = $(bouaruDescription);
+		},
+		mouseenter: function(){
+			if($(this).attr('status') != 'clicked'){
+				$(this).css({'-webkit-filter': 'grayscale(0%)','filter': 'grayscale(0%)'});
+			}
+		},
+		mouseleave: function(){
+			if($(this).attr('status') != 'clicked'){
+				$(this).css({'-webkit-filter': 'grayscale(80%)','filter': 'grayscale(80%)'});
+			}
+		}
+	});
+
+	$('#digital-waves').on({
+		click: function(){
+			$(this).attr('status', 'clicked');
+			$(defaultActiveImage).removeClass('.active-image').attr('status','unclicked').css({'-webkit-filter': 'grayscale(80%)','filter': 'grayscale(80%)'});
+			defaultActiveImage = $(this);
+			$(this).css({'-webkit-filter': 'grayscale(0%)','filter': 'grayscale(0%)'}).addClass('.active-image');
+
+			defaultDescription.stop(true, true).hide().removeClass('.active-description');
+			$(digitalDescription).stop(true, true).fadeIn(2000).show().addClass('.active-description');
+			defaultDescription = $(digitalDescription);
+		},
+		mouseenter: function(){
+			if($(this).attr('status') != 'clicked'){
+				$(this).css({'-webkit-filter': 'grayscale(0%)','filter': 'grayscale(0%)'});
+			}
+		},
+		mouseleave: function(){
+			if($(this).attr('status') != 'clicked'){
+				$(this).css({'-webkit-filter': 'grayscale(80%)','filter': 'grayscale(80%)'});
+			}
 		}
 	});
 });

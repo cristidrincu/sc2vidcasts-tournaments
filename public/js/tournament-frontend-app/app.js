4e12e5d3f633e643501679f79ee037e7083d6d50
Cristian Sergiu Drincu <cristidrincu@gmail.com>
2015-03-02 12:41:53 +0200

    Implemented contact page for frontend app

--- public/js/tournament-frontend-app/app.js
+++ public/js/tournament-frontend-app/app.js
@@ -1,7 +1,14 @@
 /**
  * Created by cristiandrincu on 2/22/15.
  */
-angular.module('tournament-frontend-app', ['tournaments.homeModule', 'tournaments.aboutModule', 'tournaments.playersModule', 'tournaments.organizersModule', 'tournaments.tournamentsModule'])
+angular.module('tournament-frontend-app',
+		['tournaments.homeModule',
+			'tournaments.aboutModule',
+			'tournaments.playersModule',
+			'tournaments.organizersModule',
+			'tournaments.tournamentsModule',
+			'tournaments.contactModule'
+		])
 .run(['$state', function($state){
 		$state.go('home');
 }])

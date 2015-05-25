377cc5e20e27819a5e492be63ec734606569c6a7
Cristian Drincu <cristidrincu@gmail.com>
2015-05-25 19:53:48 +0300

    Fixed tab identation for comment model file

--- app/models/comment.js
+++ app/models/comment.js
@@ -3,9 +3,9 @@ var user = require('./user');
 var tournament = require('./tournament');
 
 var commentSchema = mongoose.Schema({
-  commentText: String,
-	parentTournament: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tournament' }],
-  user:[{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]
+    commentText: String,
+    parentTournament: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tournament' }],
+    user:[{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]
 });
 
 module.exports = mongoose.model('Comment', commentSchema);
